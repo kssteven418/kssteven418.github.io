c21894a0c9340406366407f93464a56e075bad09
Sehoon Kim <sehoonkim@berkeley.edu>
2024-04-12 00:47:57 -0700

    temp

--- js/selectedPublications.js
+++ js/selectedPublications.js
@@ -34,25 +34,45 @@ document.addEventListener("DOMContentLoaded", function () {
   publications.filter((pub) => pub.selected).forEach(addPublication);
 
   const container2 = document.getElementById("publications-container"); // Ensure you have a div with this ID in your HTML
-
+  const fragment = document.createDocumentFragment();
   publications.forEach((publication) => {
-    const publicationHtml = `
-                <p class="text-large" style="margin-bottom:0;">
-                    <a href="${publication.pdfLink}">
-                        <b>${publication.title}</b></a>
-                    ${Object.entries(publication.codeLinks)
-                      .map(([key, url]) => `<a href="${url}">[${key}]</a>`)
-                      .join(" ")}
-                </p>
-                <p class="text" style="margin : 0; padding-top:0;">
-                    ${publication.authors}<br>
-                    <i>${publication.conference}</i>
-                </p>
-            `;
-
-    // Create a div to hold the publication HTML
     const div = document.createElement("div");
+    const publicationHtml = `
+        <p class="text-large" style="margin-bottom:0;">
+            <a href="${publication.pdfLink}">
+                <b>${publication.title}</b></a>
+            ${Object.entries(publication.codeLinks)
+              .map(([key, url]) => `<a href="${url}">[${key}]</a>`)
+              .join(" ")}
+        </p>
+        <p class="text" style="margin : 0; padding-top:0;">
+            ${publication.authors}<br>
+            <i>${publication.conference}</i>
+        </p>
+    `;
     div.innerHTML = publicationHtml;
-    container2.appendChild(div);
+    fragment.appendChild(div);
   });
+  container2.appendChild(fragment);
+
+  // publications.forEach((publication) => {
+  //   const publicationHtml = `
+  //               <p class="text-large" style="margin-bottom:0;">
+  //                   <a href="${publication.pdfLink}">
+  //                       <b>${publication.title}</b></a>
+  //                   ${Object.entries(publication.codeLinks)
+  //                     .map(([key, url]) => `<a href="${url}">[${key}]</a>`)
+  //                     .join(" ")}
+  //               </p>
+  //               <p class="text" style="margin : 0; padding-top:0;">
+  //                   ${publication.authors}<br>
+  //                   <i>${publication.conference}</i>
+  //               </p>
+  //           `;
+
+  //   // Create a div to hold the publication HTML
+  //   const div = document.createElement("div");
+  //   div.innerHTML = publicationHtml;
+  //   container2.appendChild(div);
+  // });
 });
