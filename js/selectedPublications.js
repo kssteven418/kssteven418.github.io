1a6de9fd7fe769ded186878078d6c0d032cb6105
Sehoon Kim <sehoonkim@berkeley.edu>
2024-04-12 17:26:09 -0700

    buttons

--- js/selectedPublications.js
+++ js/selectedPublications.js
@@ -5,9 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById(
       "selected-publications-container"
     );
-    const codeLinksHtml = Object.entries(item.codeLinks)
-      .map(([key, url]) => `<a href="${url}">[${key}]</a>`)
-      .join(" ");
+    const codeLinksButton = Object.entries(item.codeLinks)
+      .map(
+        ([key, url]) =>
+          `<a class="btn btn-outline-primary btn-page-header" href="${url}" target="_blank" rel="noopener">${key}</a>`
+      )
+      .join("\n");
 
     const htmlContent = `
             <div class="flex-row">
@@ -17,12 +20,17 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="flex-item flex-item-stretch-3 flex-column">
                     <p class="text-large" style="margin-bottom:0;">
                         <a href="${item.pdfLink}"><b>${item.title}</b></a>
-                        ${codeLinksHtml}
+                    </p>
+                    <p class="text" style="margin:0; padding-top:0; font-size:11pt">
+                        ${item.authors}<br>
                     </p>
                     <p class="text" style="margin:0; padding-top:0;">
-                        ${item.authors}<br>
-                        <i>${item.conference}</i>
+                        ${item.conference}
                     </p>
+                    <div class="button-container">
+                        ${codeLinksButton}
+                    </div>
+
                 </div>
             </div>
             <div><p class="text"></p></div>
@@ -41,38 +49,23 @@ document.addEventListener("DOMContentLoaded", function () {
         <p class="text-large" style="margin-bottom:0;">
             <a href="${publication.pdfLink}">
                 <b>${publication.title}</b></a>
-            ${Object.entries(publication.codeLinks)
-              .map(([key, url]) => `<a href="${url}">[${key}]</a>`)
-              .join(" ")}
         </p>
-        <p class="text" style="margin : 0; padding-top:0;">
+        <p class="text" style="margin:0; padding-top:0; font-size:11pt">
             ${publication.authors}<br>
-            <i>${publication.conference}</i>
         </p>
+        <p class="text" style="margin:0; padding-top:0;">
+            ${publication.conference}
+        </p>
+        ${Object.entries(publication.codeLinks)
+          .map(
+            ([key, url]) =>
+              `<a class="btn btn-outline-primary btn-page-header" href="${url}" target="_blank" rel="noopener">${key}</a>`
+          )
+          .join("\n")}
+
     `;
     div.innerHTML = publicationHtml;
     fragment.appendChild(div);
   });
   container2.appendChild(fragment);
-
-  // publications.forEach((publication) => {
-  //   const publicationHtml = `
-  //               <p class="text-large" style="margin-bottom:0;">
-  //                   <a href="${publication.pdfLink}">
-  //                       <b>${publication.title}</b></a>
-  //                   ${Object.entries(publication.codeLinks)
-  //                     .map(([key, url]) => `<a href="${url}">[${key}]</a>`)
-  //                     .join(" ")}
-  //               </p>
-  //               <p class="text" style="margin : 0; padding-top:0;">
-  //                   ${publication.authors}<br>
-  //                   <i>${publication.conference}</i>
-  //               </p>
-  //           `;
-
-  //   // Create a div to hold the publication HTML
-  //   const div = document.createElement("div");
-  //   div.innerHTML = publicationHtml;
-  //   container2.appendChild(div);
-  // });
 });
